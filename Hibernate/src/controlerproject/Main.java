185e1ab2efef21fba9598145853bd205746094f4
Carlos del Cerro <carlosc7fuen@gmail.com>
2015-02-19 17:36:29 +0100

    Actividad 4: Introducir preguntas y respuestas

--- Hibernate/src/controlerproject/Main.java
+++ Hibernate/src/controlerproject/Main.java
@@ -1,11 +1,45 @@
 package controlerproject;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		HibernateHelper hibernateHelper = new HibernateHelper();
-		System.out.println("Inserto una pregunta nueva");
+		Scanner scanner = new Scanner(System.in);
+		int op = scanner.nextInt();
+		
+		System.out.println("------------ELIGE UNA OPCIÓN DEL MENÚ------------");
+		System.out.println("-------------------------------------------------");
+		System.out.println("1. - Insertar una pregunta");
+		System.out.println("2. - Insertar una respuesta");
+		System.out.println("3. - SALIR");
 
-		hibernateHelper.addQuestion((byte) 2, "¿Cuánto son 2 + 2?",
-				"Matemáticas");
+		try { 
+			switch(op){ 
+		
+			case(1): 
+				System.out.print("Introduce la pregunta que quieras guardar: ");
+				p1=entrada.readLine();  
+				System.out.println("Su respuesta ha sido guardada");
+			break; 
+		
+			case(2): 
+				System.out.print("Introduce la respuesta a esa pregunta: ");
+				r1=entrada.readLine();
+				System.out.print("Su respuesta ha sido guardada");
+				break; 
+			
+			case(3): 
+				System.out.print("YA NO PUEDES INTRODUCIR PREGUNTAS/RESPUESTAS");
+				System.out.println("SALIENDO DEL PROGRAMA");
+		break; 
+		default: 
+		System.out.print("Elección Incorrecta");
+ 	} 
+		//catch(Exception error) 
+		//{ 		///System.out.println("genero error: "+error); 
+		//} 
+
+		}
 	}
 }
